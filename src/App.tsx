851a6d447a3fbf38d2a39c4d72e6b186e1423c7a
Tharmapalan Nevaashahan <79234817+Nevaashahan@users.noreply.github.com>
2025-10-27 21:42:44 +0530

    check5

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ import StudentTranscript from "./pages/Students/Studenttranscriptpage/StudentTra
 import StudentTranscriptRequestForm from "./pages/Students/StuentstranscriptApplicationPage/StuTraAppPage";
 import TranscripStatus from "./pages/Students/stuTraStatus/StuTraStatus";
 
-// Shared / users
+// Shared / user
 import AccountSettings from "./pages/UserProfileSetting/userprofilesetting";
 import UserDropdown from "./components/UserDropdown/userdropdown";
 
