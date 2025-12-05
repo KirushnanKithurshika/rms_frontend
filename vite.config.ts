d383c9fcafea9f60ff05e9b2bdb0d08798f337c4
Tharmapalan Nevaashahan <79234817+Nevaashahan@users.noreply.github.com>
2025-12-06 02:09:36 +0530

    backend api setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 5174,
     proxy: {
       "/api": {
-        target: "http://10.77.150.95:8080",
+        target: "http://10.77.150.95:8087",
         changeOrigin: true,
       },
     },
