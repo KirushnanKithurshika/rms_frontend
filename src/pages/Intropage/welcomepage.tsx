01b2037311fe07bd2113b4a243e157425b40b0ec
kithurshika Kirushnan <kirushnankithurshika@gmail.com>
2025-12-24 18:18:25 +0530

    new

--- src/pages/Intropage/welcomepage.tsx
+++ src/pages/Intropage/welcomepage.tsx
@@ -12,7 +12,7 @@ const WelcomePage: React.FC = () => {
           <img src={welcomeImage} alt="Welcome Illustration" className="welcome-img" />
         </div>
         <div className="welcome-right">
-          <h2>Welcome to the Results Management System !</h2>
+          <h2>Welcome to the RMS !</h2>
           <p>
             We warmly welcome you to the official Results Management System of Faculty of Engineering, University of Ruhuna. This platform is designed to provide students, academic staff, and administrators with secure and convenient access to academic results and performance records.
           </p>
